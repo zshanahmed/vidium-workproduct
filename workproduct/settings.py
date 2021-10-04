9b2998969228df5f5038fd34eab606cacc576ebf
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-10-03 23:26:51 -0500

    Changed debug

--- workproduct/settings.py
+++ workproduct/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG_VALUE']
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
