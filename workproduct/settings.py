0e4eb4bce84a20a01cc3711cfbcb3894ef0d6737
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-10-03 23:23:40 -0500

    Updated setting for production

--- workproduct/settings.py
+++ workproduct/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = os.environ['DEBUG_VALUE']
 
 ALLOWED_HOSTS = []
 
